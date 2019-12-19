6a8164423c1775b272db3f98bbcdec0b46c631e4
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-12-19 15:29:47 +0100

    create bike functional

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+import flatpickr from "flatpickr";
+
+const datepickers = flatpickr(".datepicker", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+  minDate: "today"
+});
